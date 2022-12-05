643c8c41696c1860b6a0eaf2468fbd7ae326c5a9
Arnas.Kazlauskas <arnas.kazlauskas@kilo.health>
2022-12-05 12:49:57 +0200

    feat(calories): solution

--- day-1-calorie-counting/calories.go
+++ day-1-calorie-counting/calories.go
@@ -1,1 +1,43 @@
 package calories
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Calories uint
+
+func Run() {
+	lines := readInput()
+	elves := make(map[int]Calories)
+	c := 0
+	max := Calories(0)
+	for _, v := range lines {
+		if v == "" {
+			c++
+			continue
+		}
+
+		i, err := strconv.Atoi(v)
+		if err != nil {
+			panic(err)
+		}
+		elves[c] += Calories(i)
+
+		if elves[c] > max {
+			max = elves[c]
+		}
+	}
+
+	fmt.Println(max)
+}
+
+func readInput() []string {
+	d, err := os.ReadFile("day-1-calorie-counting/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	return strings.Split(string(d), "\n")
+}
