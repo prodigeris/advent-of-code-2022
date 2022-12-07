698fad11ed70ce410069a84812b3c1b9b530227d
Arnas.Kazlauskas <arnas.kazlauskas@kilo.health>
2022-12-07 14:57:40 +0200

    feat(day-3-supply-stack): sum

--- day-5-supply-stack/supply-stack.go
+++ day-5-supply-stack/supply-stack.go
@@ -1,9 +1,6 @@
 package day_5_supply_stack
 
-import (
-	"regexp"
-	"strconv"
-)
+import "fmt"
 
 type action struct {
 	amount int
@@ -12,64 +9,24 @@ type action struct {
 }
 
 func Run() {
-
 }
 
-func parse(l []string) (map[int][]string, []action) {
-	stacksInput := make([]string, 0)
-	actionsInput := make([]string, 0)
-	isActions := false
-	for _, v := range l {
-		if v == "" {
-			isActions = true
-			continue
-		}
-		if isActions {
-			actionsInput = append(actionsInput, v)
-		} else {
-			stacksInput = append(stacksInput, v)
+func calc(i []string) string {
+	stacks, actions := parse(i)
+
+	for _, a := range actions {
+		for i := 0; i < a.amount; i++ {
+			stacks[a.to] = append([]string{stacks[a.from][0]}, stacks[a.to]...)
+			stacks[a.from] = stacks[a.from][1:]
 		}
 	}
 
-	return parseStacks(stacksInput), parseActions(actionsInput)
-}
+	fmt.Println(stacks)
 
-func parseStacks(i []string) map[int][]string {
-	s := make(map[int][]string)
-	for _, v := range i {
-		for i := 0; i < 12; i += 4 {
-			re := regexp.MustCompile(`(\[|\]|\s|\d)`)
-			l := re.ReplaceAllString(v[i:i+3], "")
-			if l == "" {
-				continue
-			}
-			k := i%3 + 1
-			if _, ok := s[k]; !ok {
-				s[k] = []string{}
-			}
-			s[k] = append(s[k], l)
-		}
+	s := ""
+	for i := 1; i <= len(stacks); i++ {
+		s += stacks[i][0]
 	}
+
 	return s
-}
-
-func parseActions(a []string) (r []action) {
-	for _, v := range a {
-		re := regexp.MustCompile(`move (.?) from (.?) to (.?)`)
-		match := re.FindStringSubmatch(v)
-		amount, err1 := strconv.Atoi(match[1])
-		from, err2 := strconv.Atoi(match[2])
-		to, err3 := strconv.Atoi(match[3])
-
-		if err1 != nil || err2 != nil || err3 != nil {
-			panic("Cannot convert string to int")
-		}
-
-		r = append(r, action{amount, from, to})
-	}
-	return
-}
-
-func calc(i []string, i2 []string) string {
-	return ""
 }
--- day-5-supply-stack/supply-stack_test.go
+++ day-5-supply-stack/supply-stack_test.go
@@ -34,24 +34,22 @@ func TestParsesInputCorrectly(t *testing.T) {
 
 }
 
-//func TestSumFullyContain(t *testing.T) {
-//	input := []string{
-//		"[D]",
-//		"[N] [C]",
-//		"[Z] [M] [P]",
-//	}
-//
-//	input2 := []string{
-//		"move 1 from 2 to 1",
-//		"move 3 from 1 to 3",
-//		"move 2 from 2 to 1",
-//		"move 1 from 1 to 2",
-//	}
-//
-//	actual := calc(input, input2)
-//	expected := "CMZ"
-//	if actual != expected {
-//		t.Errorf("Expected String(%s) is not same as"+
-//			" actual string (%s)", expected, actual)
-//	}
-//}
+func TestSumFullyContain(t *testing.T) {
+	input := []string{
+		"    [D]    ",
+		"[N] [C]    ",
+		"[Z] [M] [P]",
+		" 1   2   3 ",
+		"",
+		"move 1 from 2 to 1",
+		"move 3 from 1 to 3",
+		"move 2 from 2 to 1",
+		"move 1 from 1 to 2"}
+
+	actual := calc(input)
+	expected := "CMZ"
+	if actual != expected {
+		t.Errorf("Expected String(%s) is not same as"+
+			" actual string (%s)", expected, actual)
+	}
+}
