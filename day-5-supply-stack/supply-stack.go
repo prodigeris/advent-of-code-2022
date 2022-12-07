b7ef06ac67846feaaa04f025ace566527eeb5ec2
Arnas.Kazlauskas <arnas.kazlauskas@kilo.health>
2022-12-07 14:07:12 +0200

    feat(day-3-supply-stack): test parsing

--- day-5-supply-stack/supply-stack.go
+++ day-5-supply-stack/supply-stack.go
@@ -1,7 +1,23 @@
 package day_5_supply_stack
 
+import "fmt"
+
+type action struct {
+	amount int
+	from   int
+	to     int
+}
+
 func Run() {
 
+}
+
+func parse(l []string) ([][]rune, []action) {
+	for _, v := range l {
+		fmt.Println(v)
+	}
+
+	return [][]rune{}, []action{}
 }
 
 func calc(i []string, i2 []string) string {
--- day-5-supply-stack/supply-stack_test.go
+++ day-5-supply-stack/supply-stack_test.go
@@ -1,25 +1,56 @@
 package day_5_supply_stack
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestSumFullyContain(t *testing.T) {
-	input := []string{
-		"[D]",
+func TestParsesInputCorrectly(t *testing.T) {
+	input := []string{"[D]",
 		"[N] [C]",
 		"[Z] [M] [P]",
-	}
-
-	input2 := []string{
+		"1   2   3",
+		"",
 		"move 1 from 2 to 1",
 		"move 3 from 1 to 3",
 		"move 2 from 2 to 1",
-		"move 1 from 1 to 2",
+		"move 1 from 1 to 2"}
+
+	expectedStacks := [][]rune{{'N', 'Z'}, {'D', 'C', 'M'}, {'P'}}
+	expectedActions := []action{{1, 2, 1}, {3, 1, 3}, {2, 2, 1}, {1, 1, 2}}
+
+	stacks, actions := parse(input)
+
+	if !reflect.DeepEqual(expectedStacks, stacks) {
+		t.Errorf("Expected [][]rune(%v) is not same as"+
+			" actual [][]rune (%v)", expectedStacks, stacks)
 	}
 
-	actual := calc(input, input2)
-	expected := "CMZ"
-	if actual != expected {
-		t.Errorf("Expected String(%s) is not same as"+
-			" actual string (%s)", expected, actual)
+	if !reflect.DeepEqual(expectedActions, actions) {
+		t.Errorf("Expected []action(%v) is not same as"+
+			" actual []action (%v)", expectedActions, actions)
 	}
+
 }
+
+//func TestSumFullyContain(t *testing.T) {
+//	input := []string{
+//		"[D]",
+//		"[N] [C]",
+//		"[Z] [M] [P]",
+//	}
+//
+//	input2 := []string{
+//		"move 1 from 2 to 1",
+//		"move 3 from 1 to 3",
+//		"move 2 from 2 to 1",
+//		"move 1 from 1 to 2",
+//	}
+//
+//	actual := calc(input, input2)
+//	expected := "CMZ"
+//	if actual != expected {
+//		t.Errorf("Expected String(%s) is not same as"+
+//			" actual string (%s)", expected, actual)
+//	}
+//}
